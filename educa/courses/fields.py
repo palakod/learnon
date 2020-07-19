fd7151e17d0ac22588cd29b56fee39959fdc91a6
Bhanu Prakash Palakodeti <bhanu8262@gmail.com>
2020-07-19 17:51:20 +0530

    Building APIs.

--- educa/courses/fields.py
+++ educa/courses/fields.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
+
 class OrderField(models.PositiveIntegerField):
     def __init__(self, for_fields=None, *args, **kwargs):
         self.for_fields = for_fields
@@ -8,21 +9,21 @@ class OrderField(models.PositiveIntegerField):
 
     def pre_save(self, model_instance, add):
         if getattr(model_instance, self.attname) is None:
-            #no current value
+            # no current value
             try:
                 qs = self.model.objects.all()
                 if self.for_fields:
-                    # filter by objects with the same field values 
+                    # filter by objects with the same field values
                     # for the fields in "for_fields"
-                    query  = {field: getattr(model_instance, field) for field in self.for_fields}
-                    qs =qs.filter(**query)
-                # get the order for the lastitem
+                    query = {field: getattr(model_instance, field) for field in self.for_fields}
+                    qs = qs.filter(**query)
+                # get the order of the last item
                 last_item = qs.latest(self.attname)
                 value = last_item.order + 1
             except ObjectDoesNotExist:
-                value= 0
+                value = 0
             setattr(model_instance, self.attname, value)
             return value
         else:
-            return super(OrderField, self).pre_save(model_instance, add)
-
+            return super(OrderField,
+                         self).pre_save(model_instance, add)
--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('mine/',
@@ -37,13 +36,11 @@ urlpatterns = [
     path('content/order/',
          views.ContentOrderView.as_view(),
          name='content_order'),
-     path('subject/<slug:subject>)/',
+
+    path('subject/<slug:subject>)/',
          views.CourseListView.as_view(),
          name='course_list_subject'),
     path('<slug:slug>/',
          views.CourseDetailView.as_view(),
          name='course_detail'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
